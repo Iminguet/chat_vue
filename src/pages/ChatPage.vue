062e6ef120239d6c7cd4a9096f8881a26aaed779
Iminguet <i.minguet@hotmail.com>
2023-07-12 23:50:25 +0200

    Creada las rutas y las páginas principales. Añadida navegación entre páginas.

---
+++ src/pages/ChatPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Chat Page</div>
+  <router-link to="/">Ir a Home</router-link>
+</template>
+<script>
+export default {
+  setup() {
+    // ==== DATA ====
+    // === METHODS ===
+    // ==== MQTT ====
+    // ==== COMPUTED ====
+    // ==== OTHER HOOKS ====
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>Home Page</div>
+
+  <router-link to="/chat">Ir a chat</router-link>
+</template>
+<script>
+export default {
+  setup() {
+    // ==== DATA ====
+    // === METHODS ===
+
+    const irAChatPage = () => {};
+    // ==== MQTT ====
+    // ==== COMPUTED ====
+    // ==== OTHER HOOKS ====
+    return { irAChatPage };
+  },
+};
+</script>
+<style lang="scss" scoped></style>
