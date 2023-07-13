3403fa4b13f40495fe91e5b61e4c51ab2cf0d128
Iminguet <i.minguet@hotmail.com>
2023-07-13 14:04:37 +0200

    Creado componente ChatRow, donde se mostrarán los mensajes

---
+++ src/components/ChatRow.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main-wrap" :class="{ 'user-style': propId }">
+    <div class="message-wrapper">
+      <span class="author">{{ mensaje.autor }}</span>
+      <span class="main-message"> {{ mensaje.contenido }} </span>
+      <span class="time">{{ mensaje.hora }}</span>
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  props: {
+    mensaje: Object,
+  },
+  setup() {
+    // ==== DATA ====
+
+    let propId = ref("6");
+    // === METHODS ===
+    // ==== MQTT ====
+    // ==== COMPUTED ====
+    // ==== OTHER HOOKS ====
+    return { propId };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.main-wrap {
+  .message-wrapper {
+    display: grid;
+    max-width: 90%;
+    grid-template-rows: min-content auto min-content;
+    background-color: $memberChat;
+    border-radius: 10px;
+    padding: 5px;
+    .time {
+      justify-self: end;
+    }
+  }
+}
+.user-style {
+  display: grid;
+  justify-items: end;
+  .message-wrapper {
+    background-color: $userChat;
+  }
+}
+</style>
